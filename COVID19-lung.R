621d6a2f73ed0ac0f628c3be7de555cf21f32225
Garmire group <lgarmire@gmail.com>
2020-05-12 17:00:29 -0400

    Update COVID19-lung.R

--- COVID19-lung.R
+++ COVID19-lung.R
@@ -9,5 +9,5 @@ res <- results(dds, contrast=c("condition","Series15_COVID19Lung","Series15_Heal
 data_for_drug<-data.frame(geneSymbol=row.names(res),score=res$stat)
 data_for_drug<-na.omit(data_for_drug)
 row.names(data_for_drug)<-1:nrow(data_for_drug)
-write.csv(data_for_drug,file="Series15_COVID19Lung_data_for_drug.csv",quote = F,row.names = F)
+write.csv(data_for_drug,file="COVID19-Lung_data_for_drug.csv",quote = F,row.names = F)
 
